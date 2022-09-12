fc2992409b9e1c932c4d04bd845a5be0db1f2d25
Liu Santana <lsantana8@boscdelacoma.cat>
2022-09-12 18:18:38 +0200

    tot

---
+++ valorsprimitius.php
@@ -0,0 +1,13 @@
+<?php
+$i = 12;
+$x = 2.5;
+$y = true;
+$z = ('gat','gos','jordi');
+$tipus_de_i = gettype( $i );
+$tipus_de_x = gettype( $x );
+$tipus_de_y = gettype( $y );
+$tipus_de_z = gettype( $z );
+echo "La variable \$i 
+      conté el valor $i 
+	  i és del tipus $tipus_de_i";
+?>
