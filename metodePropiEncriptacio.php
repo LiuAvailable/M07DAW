55c82f05a6098fca9e99c7ed2343d5028e4eb67a
Liu Santana <lsantana8@boscdelacoma.cat>
2022-09-19 18:23:27 +0200

    canvis 2022-09-19

--- metodePropiEncriptacio.php
+++ metodePropiEncriptacio.php
@@ -5,18 +5,34 @@ Mètode d'encriptacio;
 1 -> agafar la IP del servidor
 2 -> agafar la llargada de cada paraula
 */
-$frase = "Bon dia a tothom, això és una proba";
-
-
-$ip = getClientIP();
-
-
+$frase = "patata";
+//encrypt($frase);
+//agafem la ip del client
 function getClientIP(){
 	$ip = $_SERVER['REMOTE_ADDR'];
 	if($ip == "::1"){$ip = "127.0.0.1";}
-	return $ip;
+	return removeDotsIp($ip);
 }
+//eliminem els . de la ip (0.0.0.0 -> 0000)
 function removeDotsIp($ip){
-	
+	$ip = str_replace('.', '', $ip);
+	return str_split($ip,1);
+}
+
+function toAscii($array_str){
+	$array_str_ascii = [];
+	foreach ($array_str as $char) {
+		$array_str_ascii[] = ord($char)."a";
+	}
+	return $array_str_ascii;
+}
+
+function encrypt($str){
+	$array_str = str_split($str, 1);
+	//print_r($array_str);
+	$array_str_ascii = toAscii($array_str);
+	print_r($array_str_ascii);
+
+
 }
 ?>
