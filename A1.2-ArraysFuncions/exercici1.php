6c351d45b3ba3dff73425e427287ebec36b5066e
Liu Santana <lsantana8@boscdelacoma.cat>
2022-09-22 19:24:59 +0200

    carpeta + exercici 1

---
+++ A1.2-ArraysFuncions/exercici1.php
@@ -0,0 +1,34 @@
+<?php 
+
+$array_num = [5,6,7,8,9,10];
+print_r(factorialArray($array_num));
+
+function factorialArray($array_num){
+	//si es un array procedeix a calcular el factorial
+	//en cas contrari retorna fals
+	if(is_array($array_num)){
+		$array_factorials = [];
+		foreach ($array_num as $num) {
+			if(!is_numeric($num)){return false;}//si no es numeric, talla el programa i retorna fals
+			else{			
+				$factorial = $num;
+				factorial($num, $factorial);
+				$array_factorials[] = $factorial;
+			}
+		}
+		return $array_factorials;
+	}else{return false;}
+
+}
+/*
+acció recursiva,
+fins que no hagi multiplicat tots els numeros menors a l'inicial es cridara a ella mateixa
+*/
+function factorial($num, &$factorial){
+	if ($num > 1) {
+		$factorial = $factorial * ($num-1);
+		factorial($num-1, $factorial);
+	}
+}
+
+?>
