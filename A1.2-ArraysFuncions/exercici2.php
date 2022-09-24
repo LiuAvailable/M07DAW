e65a262223587c98117ae234b11af5e0dc5ad282
Liu Santana <lsantana8@boscdelacoma.cat>
2022-09-24 20:29:04 +0200

    adaptació per matrius rectangulars

--- A1.2-ArraysFuncions/exercici2.php
+++ A1.2-ArraysFuncions/exercici2.php
@@ -1,55 +1,68 @@
 <?php 
-$quadrat = creaMatriu(2);
+$quadrat = creaMatriu(4);
+$rectangle = creaMatriu(4,6);
+echo "<h3>Matriu Quadrada</h1>";
 echo mostraMatriu($quadrat);
 echo mostraMatriu(transposaMatriu($quadrat));
+echo "<h3>Matriu Rectangular</h1>";
+echo mostraMatriu($rectangle);
+echo mostraMatriu(transposaMatriu($rectangle));
 
+/*
+* Ha d'entrar un int
+* Si entren 2 ints permet fer rectangles 
+*
+* $n == files, $m == columnes
+*/
 function creaMatriu(int $n):array{
-	$array_matriu= [];
+	if(func_num_args()>1) $m = func_get_arg(1);
+	else $m = $n;
+	$array_matriu = [];
+	$fila = [];
 	for($i=0;$i<$n;$i++){
-		for($j=0;$j<$n;$j++){
-			if ($i == $j) {
-				$array_matriu[] = "*";
-			}elseif ($i>$j) {
-				$array_matriu[] = rand(10,20);
-			}else{
-				$array_matriu[] = $j+$i;
-			}
+		for($j=0;$j<$m;$j++){
+			if ($i == $j) $fila[$j] = "*";
+			elseif ($i>$j)	$fila[$j] = rand(10,20);
+			else $fila[$j] = $j+$i;
 		}
+		$array_matriu[] = $fila;
 	}
 	return $array_matriu;
 }
 
+/*
+* converteix les columnes de la matriu d'entrada en files
+* i les files en columnes
+*/
 function transposaMatriu(array $arr):array{
 	$arr_reverse = [];
-	$key = array_count_values($arr);
-	$key = (int)$key['*'];
-	for ($i=0; $i < $key ; $i++) { 
-		for ($j=0; $j < $key; $j++) { 
-			$arr_reverse[]= $arr[$key*$j+$i];
+	for ($i=0; $i < sizeof($arr[0]); $i++) { 
+		$fila = [];
+		for ($j=0; $j < sizeof($arr); $j++) { 
+			$fila[] = $arr[$j][$i];
 		}
+		$arr_reverse[] = $fila;
 	}
 	return $arr_reverse;
 }
 
-
-
+/*
+* retorna la matriu en format html(string)
+*/
 function mostraMatriu(array $arr):string{
 	$html = "<ul>";
-	$key = array_count_values($arr);
-	$key = (int)$key['*'];
-	for ($i=0; $i < sizeof($arr); $i=$i+$key) {
-		$li_html = "";
-		for ($j=$i; $j < $i+$key; $j++) { 
-			$li_html = $li_html."<div>". $arr[$j]."</div>";
+	for ($i=0; $i < sizeof($arr); $i++) { 
+		$fila = "";
+		for ($j=0; $j < sizeof($arr[$i]); $j++) { 
+			$fila = $fila."<div>".$arr[$i][$j]."</div>";
 		}
-		$html=$html."<li>".$li_html."</li>";
+		$html = $html."<li>".$fila."</li>";
 	}
 	return $html."</ul>";
 }
 ?>
 
-
-
+<!-- CSS -->
 <style type="text/css">
 	li
 	{
