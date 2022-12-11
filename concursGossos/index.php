34a904f533ef8c85f3b74e46eb0f534cf90c9581
Liu Santana <lsantana8@boscdelacoma.cat>
2022-12-11 14:19:32 +0100

    bd credentials

--- concursGossos/index.php
+++ concursGossos/index.php
@@ -7,8 +7,7 @@ require_once('class/fase.php');
 function connectDB(){
     try {
         $dsn = "mysql:host=localhost;dbname=gossos";
-        #$conn = new PDO($dsn, "king_Liu", "Bhkl55_piu");
-        $conn = new PDO($dsn, "root", "patata");
+        $conn = new PDO($dsn, "king_Liu", "Bhkl55_piu");
     } catch (PDOException $e){echo $e->getMessage();}
 
     return $conn;
